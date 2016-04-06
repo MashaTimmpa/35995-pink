dc8ddf1b9788081637af1b7e7ad529618431b275
Masha <timmpa@gmail.com>
2016-04-06 22:04:24 +0300

    Automatization

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,12 @@
 "use strict";
 
 module.exports = function(grunt) {
-  require("load-grunt-tasks")(grunt);
+  // require("load-grunt-tasks")(grunt);
+   // grunt.loadNpmTasks("grunt-contrib-less");
+   // grunt.loadNpmTasks("grunt-postcss");
+   // grunt.loadNpmTasks("grunt-contrib-watch");
+   // grunt.loadNpmTasks("grunt-browser-sync");
+     require("load-grunt-tasks")(grunt);
 
   grunt.initConfig({
     less: {
@@ -21,13 +26,40 @@ module.exports = function(grunt) {
             "last 2 Firefox versions",
             "last 2 Opera versions",
             "last 2 Edge versions"
-          ]})
+          ]}),
+           require("css-mqpacker")({
+                  sort: true
+                  })
         ]
       },
       style: {
         src: "css/*.css"
       }
     },
+
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "css/style.min.css": ["css/style.css"]
+        }
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+           expand: true,
+           src: ["img/**/*.{png,jpg,gif}"]
+         }]
+       }
+    },
+
 
     browserSync: {
       server: {
@@ -57,4 +89,15 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask("serve", ["browserSync", "watch"]);
+
+  grunt.registerTask("build", [
+    "less",
+    "postcss",
+    "csso",
+    "imagemin"
+
+    ]
+
+    );
+
 };
